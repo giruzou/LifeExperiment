f261d8e415b501d6e92de340e3070ee79e37c987
stmtk <13043069@gakushuin.ac.jp>
2017-05-01 12:05:11 +0900

    配列をわけた

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from random import randrange
+import pygame
+from pygame.locals import *
+import sys
 
 class Animal(object):
     def __init__(self):
@@ -28,50 +31,67 @@ class Cat(Animal):
 class Rat(Animal):
     def __init__(self):
         super(Rat, self).__init__()
-        self.limit = 10
-
-class Rat(Animal):
-    def __init__(self):
-        super().__init__()
         self.limit = 2
     
 class Field(object):
     def __init__(self):
-        self.animals = []
+        # self.animals = []
+        self.rats = []
+        self.cats = []
+        
         self.size = 100
+        pygame.init()
+        self.disp_size = (400, 300)
+        self.screen = pygame.display.set_mode(self.disp_size)
+        pygame.display.set_caption("Experiment")
 
+    """
     def add(self, ani):
         self.animals.append(ani)
+    """
+    def add_cat(self, ani):
+        self.cats.append(ani)
+    
+    def add_rat(self, ani):
+        self.rats.append(ani)
 
     def add_age(self):
+        """
         for animal in self.animals:
             animal.add_age()
+        """
+        for c in self.cats:
+            c.add_age()
+        for r in self.rats:
+            r.add_age()
 
     def delete_dead(self):
-       self.animals = [animal for animal in self.animals if animal.age >= 0] 
+       # self.animals = [animal for animal in self.animals if animal.age >= 0] 
+       self.cats = [animal for animal in self.cats if animal.age >= 0] 
+       self.rats = [animal for animal in self.rats if animal.age >= 0] 
 
     def print_age(self):
-        for animal in self.animals:
-            print(animal.get_age())
+        #for animal in self.animals:
+        #   print(animal.get_age())
+        for animal in self.cats:
+            print animal.get_age()
+        for animal in self.rats:
+            print animal.get_age()
     
-    def animals_sort(self):
-    	base = lambda animal: animal.x * self.size + animal.y
-    	self.animals = sorted(self.animals, key=base)
-    	for animal in self.animals:
-    		print base(animal)
 
 field = Field()
+
 for i in range(100):
     animal = Cat()
     animal.set_pos(randrange(field.size), randrange(field.size))
-    field.add(animal)
+    field.add_cat(animal)
+    animal = Rat()
+    animal.set_pos(randrange(field.size), randrange(field.size))
+    field.add_rat(animal)
 
-field.animals_sort()
-'''
-for i in range(11):
+for i in range(4):
      field.add_age()
 field.print_age()
-print(len(field.animals))
+print(len(field.rats))
 field.delete_dead()
-print(len(field.animals))
-'''
+print(len(field.rats))
