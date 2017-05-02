bc3295aa45be36a204fc2f63c3abc785527c8204
stmtk <13043069@gakushuin.ac.jp>
2017-05-02 13:36:54 +0900

    がめんつくった

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 class Animal(object):
     def __init__(self):
+        Animal.r = 5
         self.age = 0
 
     def set_pos(self, new_x, new_y):
@@ -27,15 +28,22 @@ class Cat(Animal):
     def __init__(self):
         super(Cat, self).__init__()
         self.limit = 10
+        Cat.color = (255, 0, 0)
+
+    def display(self, screen):
+        pygame.draw.circle(screen, Cat.color, [self.x, self.y], Animal.r)
 
 class Rat(Animal):
     def __init__(self):
         super(Rat, self).__init__()
         self.limit = 2
+        Rat.color = (0, 255, 0)
     
+    def display(self, screen):
+        pygame.draw.circle(screen, Rat.color, [self.x, self.y], Animal.r)
+
 class Field(object):
     def __init__(self):
-        # self.animals = []
         self.rats = []
         self.cats = []
         
@@ -56,27 +64,25 @@ class Field(object):
         self.rats.append(ani)
 
     def add_age(self):
-        """
-        for animal in self.animals:
-            animal.add_age()
-        """
         for c in self.cats:
             c.add_age()
         for r in self.rats:
             r.add_age()
 
     def delete_dead(self):
-       # self.animals = [animal for animal in self.animals if animal.age >= 0] 
        self.cats = [animal for animal in self.cats if animal.age >= 0] 
        self.rats = [animal for animal in self.rats if animal.age >= 0] 
 
     def print_age(self):
-        #for animal in self.animals:
-        #   print(animal.get_age())
         for animal in self.cats:
             print animal.get_age()
         for animal in self.rats:
             print animal.get_age()
+    def display(self):
+        for animal in self.cats:
+            animal.display(self.screen)
+        for animal in self.rats:
+            animal.display(self.screen)
     
 
 field = Field()
@@ -89,9 +95,20 @@ for i in range(100):
     animal.set_pos(randrange(field.size), randrange(field.size))
     field.add_rat(animal)
 
+done = False
+clock = pygame.time.Clock()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    field.screen.fill((255, 255, 255))
+    field.display()
+
+"""
 for i in range(4):
      field.add_age()
 field.print_age()
 print(len(field.rats))
 field.delete_dead()
 print(len(field.rats))
+"""
